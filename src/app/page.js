5b4bb0bb8b6f654a3ab4a2db9bda50c7c91e8cdf
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-26 02:26:56 +0900

    feat(style): mockup style

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import Microphone from "./components/Microphone";
+import PoseNetComponent from "./PoseNetComponent";
 
 export default function Home() {
   return (
     <main className="flex m-4 gap-4">
-      <section id="userMedia" className="h-full flex flex-col w-1/2">
-        <div>
-          포즈넷 있어야 할 자리~
+      <section id="userMedia" className="h-[calc(100vh-6em)] gap-4 flex flex-col w-1/2">
+        <div className="mockup-browser bg-base-300 border">
+          <h1 className="mockup-browser-toolbar">사용자 모습</h1>
+          <div className="flex align-center justify-center p-4 grow bg-base-200">
+            <PoseNetComponent />
+          </div>
         </div>
+        
         <Microphone />
       </section>
       <section id="userResult">

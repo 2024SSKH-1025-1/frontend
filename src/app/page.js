011ce563784397ef25b21e92d7a0187d8509f2d6
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-26 13:49:17 +0900

    feat(tutorial): change component when video set

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 "use client";
+import { SendSpeech } from "./actions/SendSpeech";
 import Microphone from "./components/Microphone";
+import ResultViewer from "./components/ResultViewer";
 import SpeechToText from "./components/SpeechToText";
 import PoseNetComponent from "./PoseNetComponent";
-import { useState } from "react";
+import { useState, useActionState } from "react";
 
 export default function Home() {
   const [micStream, setMicStream] = useState(null);
+  const [videoName, setVideo] = useState(null);
+  const [videoList, setVideoList] = useState([]);
+  const [result, sendScript] = useActionState(SendSpeech, null)
 
   return (
     <main className="flex m-4 gap-4 bg-base-100">
@@ -14,7 +19,10 @@ export default function Home() {
         <Microphone micStream={micStream} setMicStream={setMicStream} />
       </section>
       <section id="userResult" className="flex flex-col gap-4 w-1/2">
-        <SpeechToText micStream={micStream} />
+        {!videoName ? (<>
+          <SpeechToText micStream={micStream} action={sendScript} />
+          <ResultViewer result={result} />      
+        </>) : (<></>)}
       </section>
     </main>
   );

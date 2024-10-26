ed6c7f8343f012e412e2803d28a86d4122b16c8b
heung115 <heung115@naver.com>
2024-10-26 23:51:30 +0900

    ADD AccuracyBar

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Microphone from "./components/Microphone";
 import ResultViewer from "./components/ResultViewer";
 import SpeechToText from "./components/SpeechToText";
 import PoseNetComponent from "./components/PoseNetComponent";
+import AccuracyBar from "./components/AccuracyBar";
 import { useState, useActionState } from "react";
 import { AnimatePresence } from "framer-motion";
 import VideoViewer from "./components/VideoViewer";
@@ -17,17 +18,20 @@ export default function Home() {
   return (
     <main className="flex m-4 gap-4 bg-base-100">
       <section id="userMedia" className="h-[calc(100vh-2em)] gap-4 flex flex-col w-1/2">
-        <PoseNetComponent/>
+        <PoseNetComponent />
+        <AccuracyBar />
         <Microphone micStream={micStream} setMicStream={setMicStream} setVideo={setVideo} />
       </section>
       <section id="userResult" className="flex flex-col gap-4 w-1/2">
         <AnimatePresence>
-        {!videoList[videoIndex] ? 
-          (<>
-            <SpeechToText micStream={micStream} action={sendScript} />
-            <ResultViewer result={result} setVideo={setVideo} setVideoList={setVideoList} />      
-          </>) : 
-          (<VideoViewer name={videoIndex} list={videoList} setVideoPage={setVideo} />)}
+          {!videoList[videoIndex] ? (
+            <>
+              <SpeechToText micStream={micStream} action={sendScript} />
+              <ResultViewer result={result} setVideo={setVideo} setVideoList={setVideoList} />
+            </>
+          ) : (
+            <VideoViewer name={videoIndex} list={videoList} setVideoPage={setVideo} />
+          )}
         </AnimatePresence>
       </section>
     </main>
